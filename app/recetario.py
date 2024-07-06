c87d3842ca019f856abbc8c601054b588ea2fce4
Rafael Rodríguez <rodriguezrasulanr@gmail.com>
2024-07-06 18:31:58 -0300

    interpolación de cabecera

--- app/recetario.py
+++ app/recetario.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     title="Inicio"
     return render_template("index.html",title=title)
 
